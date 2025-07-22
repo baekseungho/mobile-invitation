a280c8e9191ad3053685e77d9400d207768bdcfc
baekseungho <vudrkd1580@naver.com>
2025-07-22 20:34:55 +0900

    25.07.22 카톡섬네일수정

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const WEDDING_INVITATION_URL =
   "https://mobile-invitation-pi.vercel.app/";
-export const KAKAOTALK_API_TOKEN = "9bf28dd2d1df7f84acb22cc7e61e57d4";
-export const KAKAOTALK_SHARE_IMAGE = "https://imgur.com/a/34hJ7oW";
+export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_SHARE_IMAGE = "https://i.imgur.com/jd685ME.jpeg";
 
 export const WEDDING_DATE = "2025년 12월 27일, 토요일 오후 16시 00분";
-export const WEDDING_LOCATION = "에스가든웨딩홀";
+export const WEDDING_LOCATION = "에스가든웨딩";
 
 export const GROOM_NAME = "백승호";
 export const GROOM_ACCOUNT_NUMBER = "우리은행 1002-965-833978";
